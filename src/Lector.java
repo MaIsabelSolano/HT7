31f232674f1e66ef88bc4406c5f90f670c29ba13
MaIsabelSolano <misolanob@gmail.com>
2021-04-15 23:05:19 -0600

    .

--- src/Lector.java
+++ src/Lector.java
@@ -1,8 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Lector {
-    public Lector(String archivo) {
+    public ArrayList<Association> Lector_Asociaciones(int idioma) throws FileNotFoundException {
+        ArrayList<Association> asociaciones = new ArrayList<>();
+        File diccionario = new File("diccionario.txt");
+        Scanner reader = new Scanner(diccionario);
+        while (reader.hasNextLine()){
+            String data = reader.nextLine();
+            String[] temp = data.split(",");
+            Association<String, String> temp_As = new Association<>(temp[idioma],data);
+        }
+        return asociaciones;
+    }
+
+    public String Lector_Expresion(){
+        String expresion = "";
+        return expresion;
     }
 }

3008d3b33e8ed1a3da638b2fe576fd8e3e8eb7f0
MaIsabelSolano <misolanob@gmail.com>
2021-04-15 21:07:10 -0600

    agregación de clases

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,13 +1,12 @@
 // An implementation of nodes for use in binary trees.
 // (c) 1998, 2001 duane a. bailey
-//Obtenido en el portal de canvas :D
 
 import java.lang.Math;
 import java.util.Iterator;
 
 /**
  * This class implements a single node of a binary tree.  It is a
- * recursive structure.  Relationships between nodes are
+ * recursive structure.  Relationships between nodes are 
  * doubly linked, with parent and child references.  Many characteristics
  * of trees may be detected with static methods.
  *
@@ -95,7 +94,7 @@ public class BinaryTree<E>
      * Get right subtree of current node
      *
      * @post Returns reference to (possibly empty) right subtree
-     *
+     * 
      * @return The right subtree of this node
      */
     public BinaryTree<E> right()
@@ -107,21 +106,21 @@ public class BinaryTree<E>
      * Get reference to parent of this node
      *
      * @post Returns reference to parent node, or null
-     *
+     * 
      * @return Reference to parent of this node
      */
     public BinaryTree<E> parent()
     {
         return parent;
     }
-
+    
     /**
      * Update the left subtree of this node.  Parent of the left subtree
      * is updated consistently.  Existing subtree is detached
      *
      * @post Sets left subtree to newLeft
      *       re-parents newLeft if not null
-     *
+     * 
      * @param newLeft The root of the new left subtree
      */
     public void setLeft(BinaryTree<E> newLeft)
@@ -138,7 +137,7 @@ public class BinaryTree<E>
      *
      * @post Sets left subtree to newRight
      *       re-parents newRight if not null
-     *
+     * 
      * @param newRight A reference to the new right subtree of this node
      */
     public void setRight(BinaryTree<E> newRight)
@@ -187,8 +186,8 @@ public class BinaryTree<E>
         else return parent().root();
     }
 
-
-
+   
+    
     /**
      * Returns true if tree is empty.
      * @post Returns true iff the tree rooted at node is empty
@@ -198,15 +197,15 @@ public class BinaryTree<E>
     {
         return val == null;
     }
-
-
-
+    
+   
+   
 
     /**
      * Determine if this node is a left child
      *
      * @post Returns true if this is a left child of parent
-     *
+     * 
      * @return True iff this node is a left child of parent
      */
     public boolean isLeftChild()
@@ -219,7 +218,7 @@ public class BinaryTree<E>
      * Determine if this node is a right child
      *
      * @post Returns true if this is a right child of parent
-     *
+     * 
      * @return True iff this node is a right child of parent
      */
     public boolean isRightChild()
@@ -232,7 +231,7 @@ public class BinaryTree<E>
      * Returns value associated with this node
      *
      * @post Returns value associated with this node
-     *
+     * 
      * @return The node's value
      */
     public E value()
@@ -261,8 +260,8 @@ public class BinaryTree<E>
         if (value() != null) result += value().hashCode();
         return result;
     }
-
-    /**
+    
+	/**
      * Compute the depth of a node.  The depth is the path length
      * from node to root
      *
@@ -274,11 +273,11 @@ public class BinaryTree<E>
         if (parent() == null) return 0;
         return 1 + parent.depth();
     }
-
+	
     /**
      * Returns a string representing the tree rooted at this node.
      * <font color="#FF0000">WARNING</font> this can be a very long string.
-     *
+     * 
      * @return A string representing the tree rooted at this node.
      */
     public String treeString(){
@@ -286,36 +285,36 @@ public class BinaryTree<E>
         for (int i=0; i < this.depth(); i++){
             s += "\t|";
         }
-
+        
         s += ("<" + val + " : " + getHand() + ">\n");
-
+        
         if (!left.isEmpty()) s += left.treeString();
         if (!right.isEmpty()) s += right.treeString();
 
         return s;
     }
-
+    
     /**
-     * Support method for {@link #toString}. Returns R if this is node
+     * Support method for {@link #toString}. Returns R if this is node 
      * is a right child, L if this node is a left child and Root if this
      * node is the root.
-     *
-     * @return R if this is node
+     * 
+     * @return R if this is node 
      * is a right child, L if this node is a left child and Root if this
      * node is the root.
      */
     private String getHand(){
         if (isRightChild()) return "R";
         if (isLeftChild()) return "L";
-        return "Root";
+        return "Root";  
     }
-
-
+    
+    
     /**
      * Returns a representation the subtree rooted at this node
      *
      * @post Returns string representation
-     *
+     * 
      * @return String representing this subtree
      */
     public String toString()
